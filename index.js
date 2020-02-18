8e8659aeb42b9cea15b3882d26fec10b31ade52a
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-18 00:26:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ return string.toLowerCase()
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase)
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase())
 }
